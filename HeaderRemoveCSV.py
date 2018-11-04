32ce28c15f4929fb73238bb626590b97bdee1337
William Griffin <40611326+evelandy@users.noreply.github.com>
2018-11-03 21:28:57 -0500

    Removes Header from .CSV Files
    This Python app works in a current directory or creates one and will find all .csv files in the directory  and remove the header from them.

---
+++ HeaderRemoveCSV.py
@@ -0,0 +1,28 @@
+import csv
+import os
+
+
+# This makes a new folder in the directory that you input and leaves the one there if one exists
+directory = input("Enter the directory for your .csv files: ")  # Enter full path for the directory w/ no quotes
+os.makedirs(directory, exist_ok=True)
+
+# Loops through the files in the directory and skips the non .csv files
+for csv_file in os.listdir('.'):
+    if not csv_file.endswith('.csv'):
+        continue
+    print("Working on {}".format(csv_file))  # Prints the finished header removal of each file
+
+    # Reads through the .csv file leaving out the first row
+    csv_rows = []
+    with open(csv_file) as csv_obj:
+        reader_obj = csv.reader(csv_obj)
+        for row in reader_obj:
+            if reader_obj.line_num == 1:
+                continue
+            csv_rows.append(row)
+
+    # Write out the csv file
+    with open(os.path.join('headerRemove', csv_file), 'w', newline='') as csv_obj:
+        csv_writer = csv.writer(csv_obj)
+        for row in csv_rows:
+            csv_writer.writerow(row)
